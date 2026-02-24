1146bc393b07d190cdc148fb7939d82d61d501eb
asadi20 <asadi20@gmail.com>
2026-02-25 00:12:24 +0330

    chore(db/accounting): add chart_of_accounts table migration

---
+++ database/migrations/2026_02_24_202021_create_chart_of_accounts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chart_of_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('code',50)->unique();
+            $table->string('name', 255);
+            $table->unsignedTinyInteger('level');
+            $table->unsignedTinyInteger('account_type_id');
+            $table->unsignedTinyInteger('parent_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chart_of_accounts');
+    }
+};
