0269fc6cf4cfd9544f89a963830f085961ac95d8
asadi20 <asadi20@gmail.com>
2026-03-26 22:47:32 +0330

    feat(accounting): update DetailAccount model with fillable and annotations

--- app/Models/Accounting/DetailAccount.php
+++ app/Models/Accounting/DetailAccount.php
@@ -3,9 +3,30 @@
 namespace App\Models\Accounting;
 
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * Class DetailAccount
+ * 
+ * @property int $id
+ * @property string $name
+ * @property int $code
+ * @property int $type_id
+ * @property string|null $national_id
+ * @property string|null $description
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon $updated_at
+ * 
+ * @property DetailAccountType $type
+ * 
+ */
 class DetailAccount extends Model
 {
+    protected $fillable =[
+        'name',
+        'code',
+        'type_id',
+        'national_id',
+        'description',
+    ];
     public function type()
     {
         return $this->belongsTo(DetailAccountType::class, 'type_id');
