b4de78ae0b4b6aa7e6024e884169e92624e7494f
asadi20 <asadi20@gmail.com>
2026-03-26 22:48:21 +0330

    feat(accounting): add fillable fields and property annotations to DetailAccountType model

--- app/Models/Accounting/DetailAccountType.php
+++ app/Models/Accounting/DetailAccountType.php
@@ -4,8 +4,19 @@ namespace App\Models\Accounting;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $code
+ * @property string $name
+ * @property string|null $description
+ */
 class DetailAccountType extends Model
 {
+    protected $fillable = [
+        'code',
+        'name',
+        'description'
+    ];
     public function detailAccounts()
     {
         return $this->hasMany(DetailAccount::class, 'type_id');
