83c7042daeefce64bf475b2e0b4bc1b355929300
asadi20 <asadi20@gmail.com>
2026-03-26 22:51:41 +0330

    feat(accounting): add resource route for DetailAccountType API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Accounting\Accounts\DetailAccountTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('accounting')->group(function(){
-    
+Route::prefix('accounting')->group(function () {
+    Route::resource('detail-account-types', DetailAccountTypeController::class);
 });
