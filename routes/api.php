931d538f8cde0b53cd8f242907fe383df8d50c61
asadi20 <asadi20@gmail.com>
2026-04-19 22:09:58 +0330

    feat(accounting): add resource routes for sub-ledger and detail account links
    - Introduce CoaDetailTypeController for managing relationships
    - Add resource route: /accounting/accounts/sub-ledgers/detail-accounts
    - Prepare API endpoints for linking sub-ledgers with detail account types

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Accounting\Accounts\ChartOfAccountController;
+use App\Http\Controllers\Api\Accounting\Accounts\CoaDetailTypeController;
 use App\Http\Controllers\Api\Accounting\Accounts\DetailAccountController;
 use App\Http\Controllers\Api\Accounting\Accounts\DetailAccountTypeController;
 use App\Http\Controllers\Api\Accounting\JournalEntries\JournalEntryController;
@@ -14,8 +15,11 @@ Route::get('/user', function (Request $request) {
 Route::prefix('accounting/accounts')->group(function () {
     Route::resource('detail-account-types', DetailAccountTypeController::class);
     Route::resource('detail-accounts', DetailAccountController::class);
+    //Route::get('sub-ledgers/detail-accounts',[DetailAccountController::class,'getAllSubWithTypeLinks']);
     Route::get('sub-ledgers/{subLedger}/detail-accounts',[DetailAccountController::class, 'getBySubLedger']);
     Route::get('sub-ledgers',[ChartOfAccountController::class, 'getAllSubLedger']);
+    // links between subledger and detailAccounts
+    Route::resource('/sub-ledgers/detail-accounts', CoaDetailTypeController::class);
 });
 
 Route::prefix('accounting/journal')->group(function(){
