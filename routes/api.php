8c1f2cba79160eb031f391537440995f2bb6c7ae
asadi20 <asadi20@gmail.com>
2026-04-25 23:01:04 +0330

    feat(rbac): add API routes for role management

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\Accounting\Accounts\CoaDetailTypeController;
 use App\Http\Controllers\Api\Accounting\Accounts\DetailAccountController;
 use App\Http\Controllers\Api\Accounting\Accounts\DetailAccountTypeController;
 use App\Http\Controllers\Api\Accounting\JournalEntries\JournalEntryController;
+use App\Http\Controllers\Api\RBAC\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,12 +17,16 @@ Route::prefix('accounting/accounts')->group(function () {
     Route::resource('detail-account-types', DetailAccountTypeController::class);
     Route::resource('detail-accounts', DetailAccountController::class);
     //Route::get('sub-ledgers/detail-accounts',[DetailAccountController::class,'getAllSubWithTypeLinks']);
-    Route::get('sub-ledgers/{subLedger}/detail-accounts',[DetailAccountController::class, 'getBySubLedger']);
-    Route::get('sub-ledgers',[ChartOfAccountController::class, 'getAllSubLedger']);
+    Route::get('sub-ledgers/{subLedger}/detail-accounts', [DetailAccountController::class, 'getBySubLedger']);
+    Route::get('sub-ledgers', [ChartOfAccountController::class, 'getAllSubLedger']);
     // links between subledger and detailAccounts
     Route::resource('/sub-ledgers/detail-accounts', CoaDetailTypeController::class);
 });
 
-Route::prefix('accounting/journal')->group(function(){
+Route::prefix('accounting/journal')->group(function () {
     Route::resource('entries', JournalEntryController::class);
+});
+
+Route::prefix('rbac')->group(function () {
+    Route::resource('roles', RoleController::class);
 });
