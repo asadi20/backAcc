78ea52abe69229149e696091cbe5536937e8ec3e
asadi20 <asadi20@gmail.com>
2026-06-20 16:12:59 +0330

    test(auth): add feature tests for user registration and validation errors.

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -4,6 +4,41 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
-test('User can register with valid data', function(){
+test('User can register with valid data', function () {
+    $response = $this->postJson('/api/auth/register', [
+        'user_name' => 'testUser',
+        'name' => 'testName',
+        'email' => 'test@email.com',
+        'password' => 'password123'
+    ]);
 
+    $response->assertStatus(201)
+        ->assertJson([
+            'success' => true
+        ])->assertJsonStructure([
+                'success',
+                'message',
+                'data',
+                'error'
+            ]);
+
+    $this->assertDatabaseHas('users', [
+        'user_name' => 'testUser',
+        'email' => 'test@email.com'
+    ]);
+
+    $this->assertAuthenticated();
+});
+
+test('register validation errors', function () {
+    $response = $this->postJson('/api/auth/register', []);
+
+    $response
+        ->assertStatus(422)
+        ->assertJsonValidationErrors([
+            'user_name',
+            'name',
+            'email',
+            'password'
+        ]);
 });
