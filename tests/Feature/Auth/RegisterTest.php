10a133e48422b8a490108cbf43610fb9fa0efe27
asadi20 <asadi20@gmail.com>
2026-06-21 16:59:33 +0330

    test(auth): expand registration tests with validation cases.

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 
 uses(RefreshDatabase::class);
 
@@ -28,6 +30,10 @@ test('User can register with valid data', function () {
     ]);
 
     $this->assertAuthenticated();
+    // check password hashed or not
+    $user = User::firstOrFail();
+
+    $this->assertTrue(Hash::check('password123',$user->password));
 });
 
 test('register validation errors', function () {
@@ -41,4 +47,53 @@ test('register validation errors', function () {
             'email',
             'password'
         ]);
+});
+
+test('user cannot register with duplicate user name', function () {
+    $user = User::factory()->create([
+        'user_name' => 'user1'
+    ]);
+
+
+    $response = $this->postJson('/api/auth/register', [
+        'user_name' => 'user1',
+        'name' => 'name1',
+        'email' => 'user1@email.com',
+        'password' => 'password123'
+    ]);
+
+    $response
+        ->assertStatus(422)
+        ->assertJsonValidationErrors(['user_name']);
+
+});
+
+test('user cannot register with duplicate email', function () {
+    User::factory()->create([
+        'email' => 'user1@email.com'
+    ]);
+
+    $response = $this->postJson('/api/auth/register', [
+        'user_name' => 'user1',
+        'name' => 'name1',
+        'email' => 'user1@email.com',
+        'password' => 'password123'
+    ]);
+
+    $response
+        ->assertStatus(422)
+        ->assertJsonValidationErrors(['email']);
+});
+
+test('password must be at least 6 characters', function () {
+    $response = $this->postJson('/api/auth/register', [
+        'user_name' => 'user1',
+        'name' => 'name1',
+        'email' => 'user1@email.com',
+        'password' => '123'
+    ]);
+
+    $response
+        ->assertStatus(422)
+        ->assertJsonValidationErrors(['password']);
 });
