f8d6a632cee23e1fae1f04087282acf65e8a56dd
asadi20 <asadi20@gmail.com>
2026-07-08 14:49:20 +0330

    feat(rbac): add show and update methods to PermissionController

--- app/Http/Controllers/Api/RBAC/PermissionController.php
+++ app/Http/Controllers/Api/RBAC/PermissionController.php
@@ -13,28 +13,53 @@ class PermissionController extends Controller
     {
         $perms = Permission::all();
         return response()->json([
-            'success'=>true,
-            'data'=> $perms,
-            'message'=> 'All permsissions retrived successfully.'
+            'success' => true,
+            'data' => $perms,
+            'message' => 'All permsissions retrived successfully.'
         ], 200);
     }
 
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'name'=>'required',Rule::unique('permissions')->where(function ($query) use ($request){
-                return $query->where('guard_name', $request->guard_name);  
+            'name' => 'required',
+            Rule::unique('permissions')->where(function ($query) use ($request) {
+                return $query->where('guard_name', $request->guard_name);
             }),
-            'guard_name'=>'required|string'
+            'guard_name' => 'required|string'
         ]);
 
         $perm = Permission::create($validated);
 
         return response()->json([
-            'success'=>true,
-            'data'=> $perm,
-            'message'=>'a permission created successfully.'
+            'success' => true,
+            'data' => $perm,
+            'message' => 'a permission created successfully.'
         ], 201);
     }
 
+    public function show($id)
+    {
+        $perm = Permission::findOrFail($id);
+        if ($perm) {
+            return response()->json([
+                'success' => true,
+                'data' => $perm,
+                'message' => 'requested permission retrived successfully.',
+            ]);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'name' => 'string|required|unique,permissions.id',
+            'guard_name' => 'string|required'
+        ]);
+
+        $response = Permission::update($validated);
+
+        return $response;
+
+    }
 }
