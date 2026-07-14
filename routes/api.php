deadf884f3d7813685240a668cc9345c28cc4202
asadi20 <asadi20@gmail.com>
2026-07-14 21:08:50 +0330

    fix(auth): include role and permissions in current user response.

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,7 @@ use App\Http\Controllers\Api\RBAC\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/user',[AuthController::class,'show'])->middleware('auth:sanctum');
 
 Route::prefix('accounting/accounts')->group(function () {
     Route::resource('detail-account-types', DetailAccountTypeController::class);
@@ -22,7 +20,7 @@ Route::prefix('accounting/accounts')->group(function () {
     //Route::get('sub-ledgers/detail-accounts',[DetailAccountController::class,'getAllSubWithTypeLinks']);
     Route::get('sub-ledgers/{subLedger}/detail-accounts', [DetailAccountController::class, 'getBySubLedger']);
     Route::get('sub-ledgers', [ChartOfAccountController::class, 'getAllSubLedger']);
-    // links between subledger and detailAccounts
+    // links between sub ledger and detailAccounts
     Route::resource('/sub-ledgers/detail-accounts', CoaDetailTypeController::class);
 });
 
