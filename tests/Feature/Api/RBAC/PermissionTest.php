2bb39dfc507aa220e4d280686accc44a7a460d56
asadi20 <asadi20@gmail.com>
2026-07-24 10:46:15 +0330

    test(rbac): add store validation and uniqueness tests

--- tests/Feature/Api/RBAC/PermissionTest.php
+++ tests/Feature/Api/RBAC/PermissionTest.php
@@ -6,105 +6,167 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
-it('returns all permissions', function () {
-    $user = User::factory()->create();
-    Sanctum::actingAs($user);
+describe('index', function () {
+    it('returns all permissions', function () {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
 
-    Permission::create([
-        'name' => 'users-view',
-        'guard_name' => 'api'
-    ]);
-
-    Permission::create([
-        'name' => 'users-create',
-        'guard_name' => 'api'
-    ]);
-
-    $response = $this->getJson('/api/rbac/permissions');
-
-    $response->assertStatus(200)
-        ->assertJson([
-            'success' => true,
-            'message' => 'All permissions retrieved successfully.',
-        ])
-        ->assertJsonStructure([
-            'success',
-            'data' => [
-                '*' => [
-                    'id',
-                    'name',
-                    'guard_name',
-                    'created_at',
-                    'updated_at',
-                ]
-            ],
-            'message'
-        ])
-        ->assertJsonCount(2, 'data')
-        ->assertJsonFragment([
+        Permission::create([
             'name' => 'users-view',
             'guard_name' => 'api'
-        ])
-        ->assertJsonFragment([
+        ]);
+
+        Permission::create([
             'name' => 'users-create',
             'guard_name' => 'api'
         ]);
+
+        $response = $this->getJson('/api/rbac/permissions');
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+                'message' => 'All permissions retrieved successfully.',
+            ])
+            ->assertJsonStructure([
+                'success',
+                'data' => [
+                    '*' => [
+                        'id',
+                        'name',
+                        'guard_name',
+                        'created_at',
+                        'updated_at',
+                    ]
+                ],
+                'message'
+            ])
+            ->assertJsonCount(2, 'data')
+            ->assertJsonFragment([
+                'name' => 'users-view',
+                'guard_name' => 'api'
+            ])
+            ->assertJsonFragment([
+                'name' => 'users-create',
+                'guard_name' => 'api'
+            ]);
+    });
 });
 
-it('can store a new permission with valid data', function () {
-    $user = User::factory()->create();
-    Sanctum::actingAs($user);
 
-    $payload = [
-        'name' => 'users-delete',
-        'guard_name' => 'api'
-    ];
+describe('store', function () {
+    it('can store a new permission with valid data', function () {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
 
-    $response = $this->postJson('/api/rbac/permissions', $payload);
+        $payload = [
+            'name' => 'users-delete',
+            'guard_name' => 'api'
+        ];
 
-    $response->assertStatus(201)
-        ->assertJson([
-            'success' => true,
-            'data' => [
-                'name' => 'users-delete',
-                'guard_name' => 'api',
-            ],
-            'message' => 'a permission created successfully.'
-        ])
-        ->assertJsonStructure([
-            'success',
-            'data' => ['id', 'name', 'guard_name', 'created_at', 'updated_at'],
-            'message'
+        $response = $this->postJson('/api/rbac/permissions', $payload);
+
+        $response->assertStatus(201)
+            ->assertJson([
+                'success' => true,
+                'data' => [
+                    'name' => 'users-delete',
+                    'guard_name' => 'api',
+                ],
+                'message' => 'a permission created successfully.'
+            ])
+            ->assertJsonStructure([
+                'success',
+                'data' => ['id', 'name', 'guard_name', 'created_at', 'updated_at'],
+                'message'
+            ]);
+
+        $this->assertDatabaseHas('permissions', [
+            'name' => 'users-delete',
+            'guard_name' => 'api'
+        ]);
+    });
+
+    it('prevents guests storing a permission', function () {
+        $payload = [
+            'name' => 'users-delete',
+            'guard_name' => 'api'
+        ];
+
+        $response = $this->postJson('/api/rbac/permissions', $payload);
+
+        $response->assertStatus(401); // unauthorized
+
+        $this->assertDatabaseMissing('permissions', [
+            'name' => 'users-delete'
+        ]);
+    });
+
+    it('validates required fields when creating a permission', function () {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        // send empty payload
+        $response = $this->postJson('/api/rbac/permissions', []);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['name', 'guard_name']);
+    });
+
+    it('prevents creating a duplicate permission name for the same guard', function () {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        Permission::create([
+            'name' => 'users-create',
+            'guard_name' => 'api'
         ]);
 
-    $this->assertDatabaseHas('permissions', [
-        'name' => 'users-delete',
-        'guard_name' => 'api'
-    ]);
-});
+        $payload = [
+            'name' => 'users-create',
+            'guard_name' => 'api'
+        ];
 
-it('prevents guests storing a permission', function () {
-    $payload = [
-        'name' => 'users-delete',
-        'guard_name' => 'api'
-    ];
+        $response = $this->postJson('/api/rbac/permissions', $payload);
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['name']);
+    });
 
-    $response = $this->postJson('/api/rbac/permissions', $payload);
+    it('allows creating the same permission name for different guards', function () {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
 
-    $response->assertStatus(401); // unauthorized
+        Permission::create([
+            'name' => 'users-create',
+            'guard_name' => 'api'
+        ]);
 
-    $this->assertDatabaseMissing('permissions', [
-        'name' => 'users-delete'
-    ]);
-});
+        $payload = [
+            'name' => 'users-create',
+            'guard_name' => 'web'
+        ];
 
-it('validates required fields when creating a permission', function () {
-    $user = User::factory()->create();
-    Sanctum::actingAs($user);
+        $response = $this->postJson('/api/rbac/permissions', $payload);
+        $response->assertStatus(201);
 
-    // send empty payload
-    $response = $this->postJson('/api/rbac/permissions', []);
+        $this->assertDatabaseHas('permissions', [
+            'name' => 'users-create',
+            'guard_name' => 'web'
+        ]);
+    });
 
-    $response->assertStatus(422)
-        ->assertJsonValidationErrors(['name', 'guard_name']);
+    it('validate maximum length of permission name', function () {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $payload = [
+            'name' => str_repeat('a', 256),
+            'guard_name' => 'api'
+        ];
+
+        $response = $this->postJson('/api/rbac/permissions', $payload);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['name']);
+    });
 });
